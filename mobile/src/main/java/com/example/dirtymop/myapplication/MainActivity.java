0dfb652b1ea0971ef042500cc323beb1f4b34636
Alexander DeRieux <acd1797@vt.edu>
2015-07-23 16:46:25 -0400

    TODO in MainActivity.

--- mobile/src/main/java/com/example/dirtymop/myapplication/MainActivity.java
+++ mobile/src/main/java/com/example/dirtymop/myapplication/MainActivity.java
@@ -14,6 +14,7 @@ import com.example.dirtymop.myapplication.interfaces.HomeInteractionListener;
 import com.example.dirtymop.myapplication.interfaces.MenuInteractionListener;
 
 
+// TODO: Need to implement the InteractionListener methods AFTER creating them in the interface files.
 public class MainActivity extends Activity implements HomeInteractionListener,
         MenuInteractionListener, HistoryInteractionListener {
 

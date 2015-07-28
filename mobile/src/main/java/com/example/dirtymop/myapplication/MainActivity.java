1013171c12f9dbc77fd27ebece23132ffa45d647
Alexander DeRieux <acd1797@vt.edu>
2015-07-27 23:56:34 -0400

    Implemented button and onClickListener.

--- mobile/src/main/java/com/example/dirtymop/myapplication/MainActivity.java
+++ mobile/src/main/java/com/example/dirtymop/myapplication/MainActivity.java
@@ -4,10 +4,13 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.dirtymop.myapplication.interfaces.HistoryInteractionListener;
 import com.example.dirtymop.myapplication.interfaces.HomeInteractionListener;
@@ -27,6 +30,16 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Dummy button to start home activity.
+        Button b = (Button) findViewById(R.id.homeButton);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override

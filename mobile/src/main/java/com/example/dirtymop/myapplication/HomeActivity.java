31986eeda41f85b687649d6baa9f38f1fcf635ac
Alexander DeRieux <acd1797@vt.edu>
2015-07-27 20:56:34 -0400

    Now extends Activity.

--- mobile/src/main/java/com/example/dirtymop/myapplication/HomeActivity.java
+++ mobile/src/main/java/com/example/dirtymop/myapplication/HomeActivity.java
@@ -1,12 +1,13 @@
 package com.example.dirtymop.myapplication;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class HomeActivity extends ActionBarActivity {
+public class HomeActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

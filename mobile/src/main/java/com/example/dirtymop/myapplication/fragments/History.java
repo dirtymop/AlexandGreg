e911c337af39d00a1c0220e5d45c7996c20d08a4
Alexander DeRieux <acd1797@vt.edu>
2015-07-28 21:23:10 -0400

    Removed unused comments.

--- mobile/src/main/java/com/example/dirtymop/myapplication/fragments/History.java
+++ mobile/src/main/java/com/example/dirtymop/myapplication/fragments/History.java
@@ -38,15 +38,8 @@ public class History extends Fragment {
     // XML elements
     private ListView entries;
 
-    /**
-    * Use this factory method to create a new instance of
-    * this fragment using the provided parameters.
-    *
-            * @param param1 Parameter 1.
-            * @param param2 Parameter 2.
-            * @return A new instance of fragment History.
-            */
-    // TODO: Rename and change types and number of parameters
+
+    // Creates new instance of constructor
     public static History newInstance(String param1, String param2) {
         History fragment = new History();
         Bundle args = new Bundle();
@@ -111,12 +104,5 @@ public class History extends Fragment {
         super.onDetach();
         listener = null;
     }
-
-//    private class HistoryEntryAdapter extends ArrayAdapter<HistoryEntry> implements View.OnClickListener {
-//        @Override
-//        public void onClick(View v) {
-//
-//        }
-//    }
 
 }

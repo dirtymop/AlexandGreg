ad3b4433fcc7f04cf90b9e4a33e8357fe67f7648
Alexander DeRieux <acd1797@vt.edu>
2015-07-28 23:29:37 -0400

    New element instance incorperates new constructor values.

--- mobile/src/main/java/com/example/dirtymop/myapplication/classes/HistoryEntry.java
+++ mobile/src/main/java/com/example/dirtymop/myapplication/classes/HistoryEntry.java
@@ -8,20 +8,22 @@ public class HistoryEntry {
     // Data variables
     // TODO: update fields as needed.
     public float distance;
-    public float calories;
+    public float heartrate;
     public float speed;
+    public float elevation;
     public String map;
     public float latitude, longitude;
 
 
     // Constructor
-    public HistoryEntry(String map, float latitude, float longitude, float distance, float calories, float speed) {
+    public HistoryEntry(String map, float latitude, float longitude, float distance, float heartrate, float speed, float elevation) {
         this.map = map;
         this.latitude = latitude;
         this.longitude = longitude;
         this.distance = distance;
-        this.calories = calories;
+        this.heartrate = heartrate;
         this.speed = speed;
+        this.elevation = elevation;
     }
 
 }

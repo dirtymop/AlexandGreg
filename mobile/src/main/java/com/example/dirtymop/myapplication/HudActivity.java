a62302a8bc548bb1d79344ae1bdf3280bc704c88
Alexander DeRieux <acd1797@vt.edu>
2015-08-02 16:54:37 -0400

    Start HUD from MainActivity button.

--- mobile/src/main/java/com/example/dirtymop/myapplication/HudActivity.java
+++ mobile/src/main/java/com/example/dirtymop/myapplication/HudActivity.java
@@ -1,15 +1,25 @@
 package com.example.dirtymop.myapplication;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class HudActivity extends AppCompatActivity {
+public class HudActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Make activitiy fullscreen.
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // Set view.
         setContentView(R.layout.activity_hud);
     }
 

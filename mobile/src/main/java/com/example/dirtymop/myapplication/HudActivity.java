ca0806e617824115b7f3997c4f945d004363ecd0
Alexander DeRieux <acd1797@vt.edu>
2015-08-02 18:33:57 -0400

    HUD now reads GPS location and updates lat, long, and accuracy.

--- mobile/src/main/java/com/example/dirtymop/myapplication/HudActivity.java
+++ mobile/src/main/java/com/example/dirtymop/myapplication/HudActivity.java
@@ -1,14 +1,25 @@
 package com.example.dirtymop.myapplication;
 
 import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class HudActivity extends Activity {
+
+    TextView latitude, longitude, accuracy;
+    private int LOCATION_DISTANCE_REFRESH = 0;  // meters
+    private int LOCATION_TIME_REFRESH = 500;    // milliseconds
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +32,50 @@ public class HudActivity extends Activity {
 
         // Set view.
         setContentView(R.layout.activity_hud);
+
+        // Initialize TextView
+        latitude = (TextView) findViewById(R.id.latitude);
+        longitude = (TextView) findViewById(R.id.longitude);
+        accuracy = (TextView) findViewById(R.id.accuracy);
+
+        // -----------------
+        // Location Services
+        //
+        // GPS Location Provider
+        String locationprovider = LocationManager.GPS_PROVIDER;
+
+        // Acquire a reference to the system Location Manager
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        // Define a listener that responds to location updates
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                latitude.setText("Latitude: " + String.valueOf(location.getLatitude()));
+                longitude.setText("Longitude: " + String.valueOf(location.getLongitude()));
+                accuracy.setText("Accuracy: " + String.valueOf(location.getAccuracy()));
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+                Toast.makeText(getApplicationContext(), "provider enabled: " + provider, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Toast.makeText(getApplicationContext(), "provider disabled: " + provider, Toast.LENGTH_LONG).show();
+            }
+        };
+
+        // Schedule to receive location updates on the set listener with the given refresh intervals.
+        locationManager.requestLocationUpdates(locationprovider, LOCATION_TIME_REFRESH,
+                LOCATION_DISTANCE_REFRESH, locationListener);
+
     }
 
     @Override

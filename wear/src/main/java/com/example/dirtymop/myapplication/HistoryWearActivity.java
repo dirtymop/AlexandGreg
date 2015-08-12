67af4217114edb81c6c79d6c56c950f87e6aef3c
Alexander DeRieux <acd1797@vt.edu>
2015-08-12 18:26:55 -0400

    Alex: New activity for wearable history view.

---
+++ wear/src/main/java/com/example/dirtymop/myapplication/HistoryWearActivity.java
@@ -0,0 +1,44 @@
+package com.example.dirtymop.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.wearable.activity.WearableActivity;
+import android.support.wearable.view.WatchViewStub;
+import android.widget.TextView;
+
+public class HistoryWearActivity extends WearableActivity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history_wear);
+
+        // Ambient mode
+        setAmbientEnabled();
+
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mTextView = (TextView) stub.findViewById(R.id.text);
+            }
+        });
+    }
+
+    @Override
+    public void onEnterAmbient(Bundle ambientDetails) {
+        super.onEnterAmbient(ambientDetails);
+    }
+
+    @Override
+    public void onUpdateAmbient() {
+        super.onUpdateAmbient();
+    }
+
+    @Override
+    public void onExitAmbient() {
+        super.onExitAmbient();
+    }
+}
